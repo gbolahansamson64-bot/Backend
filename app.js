413b53809025d95e081fbaba500551d765ca9282
Gbolly <gbolahansamson64@gmail.com>
2026-08-15 20:27:04 +0100

    Fix production CORS

--- app.js
+++ app.js
@@ -16,17 +16,21 @@ const adminNotificationRoutes = require("./routes/adminNotificationRoutes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://127.0.0.1:5500",
-      "http://localhost:5500",
-      "http://127.0.0.1:5501",
-      "http://localhost:5501",
-    ],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:5501",
+    "http://localhost:5501",
+    "https://blegab.com",
+    "https://www.blegab.com"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+
+app.use(cors(corsOptions));
 
 /*
 |--------------------------------------------------------------------------
